1b9fa02c5ae51527302517ec65ea0d1cf5338cb3
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-15 23:46:33 +0530

    Get userId dynamically

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 7000;
 
-app.get('/users', (req, res) =>{
+app.get('/api/users', (req, res) =>{
     return res.json(users);
 });
+
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id === id);
+    return res.json(user);
+})
 
 app.listen(PORT, () => console.log(`server is started :${PORT}`));
