ff482475fcb80d5a5e887760ea5a558339393f81
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-16 18:40:37 +0530

    add route

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ app.get('/api/users', (req, res) =>{
     return res.json(users);
 });
 
-app.get('/api/users/:id', (req, res) => {
+app.route('/api/users/:id')
+.get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user)=>user.id === id);
-    return res.json(user);
+    if (user) {
+        return res.json(user);
+    } else {
+        return res.status(404).json({ error: "User not found" });
+    }
+ 
 })
+.patch((req, res) => {
+    return res.json({status: "pending"})
+})
+.delete((req,res) => {
+    return res.json({status:"pending"})
+})
+
 
 app.listen(PORT, () => console.log(`server is started :${PORT}`));
